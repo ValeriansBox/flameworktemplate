7566852f9498ddbd82b62f298a0de25b0757afd9
ValeriansBox <neonwinternight1@gmail.com>
2024-09-15 09:12:01 +0300

    Got everything setup

---
+++ src/client/flamework.runtime.client.ts
@@ -0,0 +1,6 @@
+import { Flamework } from "@flamework/core";
+
+Flamework.addPaths("src/client/components");
+Flamework.addPaths("src/client/controllers");
+
+Flamework.ignite();
---
+++ src/server/flamework.runtime.server.ts
@@ -0,0 +1,6 @@
+import { Flamework } from "@flamework/core";
+
+Flamework.addPaths("src/server/components");
+Flamework.addPaths("src/server/services");
+
+Flamework.ignite();
